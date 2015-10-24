6a78f104f1876dcb25c48931af698652c56a24f6
Jordi Prats <jprats@systemadmin.es>
2015-10-24 11:29:39 +0200

    scriot d'inici i rename

--- scripts/runmebitch.sh
+++ scripts/runmebitch.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+if [ -f /.hari ];
+then
+  echo "already deployed, modafuca"
+  exit 1
+fi
+
+touch /.hari
+
+echo "== installing centos 5 base, bitch =="
+
+/usr/local/src/docker-deploy/basecentos5.sh
+
+echo "== installing qmail stage 1 part 1, bitch =="
+
+/usr/local/src/docker-deploy/qmail_s01p01.sh
